a1a6c654335698082c4d51b7b2a6f72029d7dded
Jeremy Huff <jeremythuff@gmail.com>
2022-10-19 22:40:30 -0500

    Added portraits and email addresses

--- client/src/components/aboutus/index.js
+++ client/src/components/aboutus/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './styles.css';
 import TeamMemberComponent from './teamMember.component';
+import timPort from '../shared/images/TimPortrait.jpg';
+import jeremyPort from '../shared/images/JeremyPortrait.jpg';
+import jeffPort from '../shared/images/JeffPortrait.jpg';
+import ryanPort from '../shared/images/RyanPortrait.jpg';
+import jacobPort from '../shared/images/Jacob.jpg';
+
+
 export default class AboutUsComponent extends React.Component {
     render() {
         return  (
@@ -15,11 +22,36 @@ export default class AboutUsComponent extends React.Component {
                     </div>
                 </div>
                 <div id="team" className='my-5 mb-5'>
-                    <TeamMemberComponent></TeamMemberComponent>
-                    <TeamMemberComponent></TeamMemberComponent>
-                    <TeamMemberComponent></TeamMemberComponent>
-                    <TeamMemberComponent></TeamMemberComponent>
-                    <TeamMemberComponent></TeamMemberComponent>
+                    <TeamMemberComponent 
+                        fullName='Tim Chockerham'
+                        email=''
+                        portrait={timPort}
+                        bio='Tim Cockerham is a proud triple-Aggie. He graduated from Texas A&M with a BBA in Management of Information Systems in 2000, has worked at Texas A&M for the last 18 years as a database administrator, and is midway through his master’s degree there as well. Before he was a DBA though, he made money as a waiter, a bartender, a radio DJ, a burger flipper, a butcher, a parking lot valet, a software developer, a teacher, a test driver, a lawn mower, and a paperboy. He’s always willing to do any job that helps out his family, friends, or colleagues.'>
+                    </TeamMemberComponent>
+                    <TeamMemberComponent 
+                        fullName='Jeremy Huff'
+                        email='huff@tamu.edu'
+                        portrait={jeremyPort}
+                        bio="Jeremy Huff is a Software Application Developer working for Texas A&M Technology Services - Libraries. He has been developing software for the Libraries at Texas A&M for the past ten years. Before joining A&M as a software developer Jeremy had a diverse set of educational and vocational experiences. His undergraduate degree was a dual bachelor's in Philosophy and Criminal Justice. After graduation, Jeremy taught Classics, Logic, and Ballroom Dancing for 4 years at the secondary level, as well as pastoring a small congregation. Jeremy is currently pursuing a Master of Science in Management Information Systems and is eagerly anticipating whatever future developments may be in store in his career.">
+                    </TeamMemberComponent>
+                    <TeamMemberComponent 
+                        fullName='Jeff Mayhall'
+                        email='jmayhall@tamu.edu'
+                        portrait={jeffPort}
+                        bio='Jeff Mayhall was born in Alabama and unconscionably raised as an Auburn fan while growing up in Texas. He eventually earned his Bachelor of Computer Science degree from Auburn University before pursuing his MS-MIS from Texas A&M. He has worked in IT for nearly 20 years and has always felt drawn toward “helping people”. Being a very introverted person with a drive to help people, he enjoys finding ways to use technology to help others who help people as his way to contribute to the community. He has a lot of pride in helping 100s of doctors and police officers with their technological needs to assist them with providing invaluable services to society and is always looking for more ways to help others do the same.'>
+                    </TeamMemberComponent>
+                    <TeamMemberComponent 
+                        fullName='Ryan Mobley'
+                        email='ryan.mobley@tamu.edu'
+                        portrait={ryanPort}
+                        bio='Ryan Mobley is the loudest and proudest member of the Fightin’ Texas Aggie Class of 2019 (Whoop)! Ryan was born in 1996 and grew up in Lake Jackson, Texas. After graduating from Texas A&M in 2019 with a BBA in Management Information Systems, Ryan chose to pursue a career in technology consulting with the public consulting firm Grant Thornton in Dallas, TX. At the end of the day, Ryan’s main goal is to let the human element of each project shine through the code and architecture so that everyday people can benefit from advancements in technology.'>
+                    </TeamMemberComponent>
+                    <TeamMemberComponent 
+                        fullName='Jacob Palisch'
+                        email='jpalisch@tamu.edu'
+                        portrait={jacobPort}
+                        bio='Although a Texas native, Jacob Palisch traveled out to California for his undergraduate degree and collegiate baseball, where he graduated with a B.S. in Computer Science from Stanford University in 2021. After graduating, he returned to Texas, where he began a graduate program in the Mays Business School at Texas A&M and played his final year of baseball. He is continuing to pursue baseball, signing a contract with the Chicago White Sox. Jacob’s ultimate motivation is to use the work ethic and teamwork skills he has developed through years of athletics to help those around him succeed and thrive in their own impactful way.'>
+                    </TeamMemberComponent>
                 </div>
             </div>
         );
--- client/src/components/aboutus/teamMember.component.js
+++ client/src/components/aboutus/teamMember.component.js
@@ -9,17 +9,14 @@ export default class TeamMemberComponent extends React.Component {
                 <div className='container'>
                     <div className='row'>
                         <div className='col-3'>
-                            <picture>
-                                <source srcset={placeholder} type="image/svg+xml" />
-                                <img src="..." className="img-fluid img-thumbnail" alt="..." />
-                            </picture>
+                            <img src={this.props.portrait || placeholder} className="img-fluid img-thumbnail" alt="..." />
                         </div>
                         <div className='col-9'>
-                            <h2>Full Name</h2>
+                            <h2>{this.props.fullName || "Full Name"}</h2>
                             <div class="bio">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam felis nunc, interdum ut efficitur vitae, suscipit eu tortor. Phasellus eu sagittis ligula, eu dictum lectus. Nunc vehicula ut massa vitae facilisis. Praesent sagittis lacus sed lacus vulputate, tincidunt auctor eros volutpat. Quisque felis nisi, auctor non dui pharetra, consectetur pretium sapien. Duis sed nisl eu quam facilisis vehicula. Cras at tortor auctor, scelerisque purus et, viverra augue. Pellentesque nulla risus, vulputate id dolor vel, rutrum euismod erat. Nam odio ipsum, tristique nec libero ac, aliquam tincidunt nulla. Integer et varius justo. Nam eget convallis nulla. Sed id aliquam urna, varius fringilla nisl. Donec vel augue vitae magna consectetur lobortis ac eu mauris. Duis consectetur tempor nibh vitae porta.</p>
+                                <p>{this.props.bio || "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam felis nunc, interdum ut efficitur vitae, suscipit eu tortor. Phasellus eu sagittis ligula, eu dictum lectus. Nunc vehicula ut massa vitae facilisis. Praesent sagittis lacus sed lacus vulputate, tincidunt auctor eros volutpat. Quisque felis nisi, auctor non dui pharetra, consectetur pretium sapien. Duis sed nisl eu quam facilisis vehicula. Cras at tortor auctor, scelerisque purus et, viverra augue. Pellentesque nulla risus, vulputate id dolor vel, rutrum euismod erat. Nam odio ipsum, tristique nec libero ac, aliquam tincidunt nulla. Integer et varius justo. Nam eget convallis nulla. Sed id aliquam urna, varius fringilla nisl. Donec vel augue vitae magna consectetur lobortis ac eu mauris. Duis consectetur tempor nibh vitae porta."}</p>
                             </div>
-                            <button className='btn btn-primary action float-end' type="button">Contact Team Member</button>
+                            <a className='btn btn-primary action float-end' href={('mailto:') + (this.props.email || "teamMember@email.com")}>Contact {this.props.fullName || "Team Member"}</a>
                         </div>
                     </div>
                 </div>
